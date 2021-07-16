7dd86e5837b60a7af227150f58fd3979dfdf350e
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-16 16:19:25 +0100

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,31 +8,37 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-   async function fetchMoviesHandler() {
-     setIsLoading(true);
-     setError(null);
-     try {
+  
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
       const response = await fetch('https://swapi.dev/api/films/')
       if (!response.ok) {
         throw new Error('Something went wrong');
       }
       const data = await response.json();
-
-
+      
+      
       const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
       });
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
   return (
     <React.Fragment>
       <section>
